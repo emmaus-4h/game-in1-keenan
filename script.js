e81340f0dff82b8bd0764389b8bf3141e9f5711d
Keenan Cheung <>
2021-05-16 16:06:11 +0000

    i made sonic move faster

--- script.js
+++ script.js
@@ -127,16 +127,16 @@ var beweegKogel = function() {
  */
 var beweegSpeler = function draw() {
    if (keyIsDown(37)) {
-    spelerX = spelerX - 1;
+    spelerX = spelerX - 5;
   }
   if (keyIsDown(38)) {
-    spelerY = spelerY + 1;
+    spelerY = spelerY - 5;
   }
   if (keyIsDown(39)) {
-    spelerX = spelerX - 1;
+    spelerX = spelerX + 5;
   }
   if (keyIsDown(40)) {
-    spelerY = spelerY + 1;
+    spelerY = spelerY + 5;
   }
 };
 
